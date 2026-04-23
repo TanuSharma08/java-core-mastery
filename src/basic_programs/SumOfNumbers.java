f177e3facb3a718cf75f798c00d61b59e6a9098c
Tanu <sharma08.ts@gmail.com>
2026-04-23 18:46:45 +0530

    Added SumOfNumbers Program

--- src/basic_programs/SumOfNumbers.java
+++ src/basic_programs/SumOfNumbers.java
@@ -1,4 +1,9 @@
 package basic_programs;
 
+// Sum of Two Numbers
 public class SumOfNumbers {
+    public static void main (String[] args){
+        int a = 10, b = 20;
+        System.out.println("Sum of " + a + " and " + b + " is " + (a + b));
+    }
 }
