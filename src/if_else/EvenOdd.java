81f365df2db952de72cb7f4142972b05a18a6a9c
Tanu <sharma08.ts@gmail.com>
2026-05-16 19:56:32 +0530

    Update Java practice programs

--- src/if_else/EvenOdd.java
+++ src/if_else/EvenOdd.java
@@ -3,7 +3,7 @@ package if_else;
 // Check whether the number is even or odd
 public class EvenOdd {
     public static void main (String[] args){
-        int num = 99;
+        int num = 98;
         if(num % 2 == 0){
             System.out.println("It is an even number");
         }
