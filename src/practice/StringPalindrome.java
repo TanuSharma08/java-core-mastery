f2f95abfc99929950559d7aa731e3a17c5d5a5b4
Tanu <sharma08.ts@gmail.com>
2026-08-13 20:11:38 +0530

    Two pointer approach for string palindrome

--- src/practice/StringPalindrome.java
+++ src/practice/StringPalindrome.java
@@ -6,16 +6,29 @@ public class StringPalindrome {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         boolean isReverse = true;
+        int left = 0;
+        int right = str.length() - 1;
 
         String rev = new StringBuilder(str).reverse().toString();
         System.out.println("Original: " + str + " " + "Reversed: " + rev);
 
-        for (int i = 0; i < str.length() / 2; i++) {
-            if(str.charAt(i) != str.charAt(str.length() - 1 - i)){
-                isReverse = false;
-                break;
-            }
-        }
+//        Using For Loop
+//        for (int i = 0; i < str.length() / 2; i++) {
+//            if(str.charAt(i) != str.charAt(str.length() - 1 - i)){
+//                isReverse = false;
+//                break;
+//            }
+//        }
+//        Using While Loop
+          while(left < right) {
+
+              if (str.charAt(left) != str.charAt(right)) {
+                  isReverse = false;
+                  break;
+              }
+              left++;
+              right--;
+          }
 
         System.out.println(isReverse ? "True" : "False");
     }
